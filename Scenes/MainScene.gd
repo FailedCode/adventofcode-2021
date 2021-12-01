7515be4b92f085ef45740e1eb82500ac6f5d1333
FailedCode <xeres666@googlemail.com>
2021-12-01 21:08:24 +0100

    center window

--- Scenes/MainScene.gd
+++ Scenes/MainScene.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 func _ready():
-	pass # Replace with function body.
+	# center window
+	var monitorId = 0
+	var screen_size = OS.get_screen_size(monitorId)
+	var window_size = OS.get_window_size()
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
