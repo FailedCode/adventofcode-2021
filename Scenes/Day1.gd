1e8cb26989c4ed01fdd3c16c587ad22e754efce0
FailedCode <xeres666@googlemail.com>
2021-12-01 21:23:22 +0100

    Day 1, part 2

--- Scenes/Day1.gd
+++ Scenes/Day1.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-var part1ResultLabel
 var puzzleInput
+var part1ResultLabel
+var part2ResultLabel
 
 func _ready():
 	part1ResultLabel = find_node("Part1Result")
+	part2ResultLabel = find_node("Part2Result")
 	puzzleInput = find_node("PuzzleInput")
 
 func _on_Part1_button_up():
@@ -14,11 +16,31 @@ func _on_Part1_button_up():
 func calculatePart1():
 	var inputText = puzzleInput.text
 	var numbers = inputText.split("\n", false);
+	var increases = diffNumbers(numbers)	
+	return String(increases)
+
+func diffNumbers(_numbers):
 	var prev = 0
 	var increases = 0
-	for i in range(1, numbers.size()):
-		if int(numbers[i]) > int(numbers[prev]):
+	for i in range(1, _numbers.size()):
+		if int(_numbers[i]) > int(_numbers[prev]):
 			increases += 1
 		prev = i
-		
+	return increases
+
+func _on_Part2_button_up():
+	var result = calculatePart2()
+	part2ResultLabel.text = result
+
+func calculatePart2():
+	var inputText = puzzleInput.text
+	var numbers = inputText.split("\n", false);
+	var prevA = 0
+	var prevB = 1
+	var sums = []
+	for i in range(2, numbers.size()):
+		sums.append(int(numbers[prevA]) + int(numbers[prevB]) + int(numbers[i]))
+		prevA += 1
+		prevB += 1
+	var increases = diffNumbers(sums)
 	return String(increases)
