e9cf15ba485a9c26d9056d8ef840012a95c8e67c
FailedCode <xeres666@googlemail.com>
2021-12-03 21:40:38 +0100

    Day 3, part 1

---
+++ Days/Day3.gd
@@ -0,0 +1,31 @@
+extends AbstractDay
+
+func calculatePart1():
+	var lines = getInputAsLines()
+	var majority = (lines.size() / 2) + 1
+	if majority % 2 == 0:
+		majority += 1
+	var values = []
+	for i in lines[0].length():
+		values.append(0)
+	for i in range(0, lines.size()):
+		var j = 0
+		for c in lines[i]:
+			if c == "1":
+				values[j] += 1
+			j += 1
+	
+	var gamma = ""
+	var epsilon = ""
+	for v in values:
+		if v >= majority:
+			gamma += "1"
+			epsilon += "0"
+		else:
+			gamma += "0"
+			epsilon += "1"
+		
+	return Utility.bin2int(gamma) * Utility.bin2int(epsilon)
+	
+func calculatePart2():
+	return ""
--- Utility.gd
+++ Utility.gd
@@ -21,3 +21,18 @@ func _deferred_change_scene(arguments):
 	get_tree().get_root().add_child(current_scene)
 	get_tree().set_current_scene(current_scene)
 	
+func bin2int(binaryString) -> int:
+	var result = 0
+	binaryString = string_reverse(binaryString)
+	for i in range(0, binaryString.length()):
+		var c = binaryString[i]
+		if c == "1":
+			result += pow(2, i)
+			
+	return result
+
+func string_reverse(string):
+	var result = ""
+	for i in range(string.length() - 1, -1, -1):
+		result += string[i]
+	return result
