961266e892e80d7fd176f47dcc08bb5db4641360
FailedCode <xeres666@googlemail.com>
2021-12-11 20:15:09 +0100

    Day 10, part 2

--- Days/Day10.gd
+++ Days/Day10.gd
@@ -6,6 +6,12 @@ var illegalCharTable = {
 	"}": 1197,
 	">": 25137,
 }
+var autocompleteCharTable = {
+	"(": 1,
+	"[": 2,
+	"{": 3,
+	"<": 4,
+}
 var openBracket = ["(", "[", "{", "<"]
 var matchedBracket = {
 	")":"(", 
@@ -26,6 +32,21 @@ func calculatePart1():
 			points += illegalCharTable[c]
 	return points
 
+func calculatePart2():
+	var lines = getInputAsLines()
+	var incompleteLines = []
+	for line in lines:
+		var c = firstIllegalChar(line)
+		if c == null:
+			incompleteLines.append(line)
+	
+	var scores = []
+	for line in incompleteLines:
+		var points = autocompletePoints(line)
+		scores.append(points)
+	scores.sort()
+	return scores[scores.size() / 2]
+
 func firstIllegalChar(line):
 	var openChar = []
 	for c in line:
@@ -35,3 +56,18 @@ func firstIllegalChar(line):
 			if openChar.pop_back() != matchedBracket[c]:
 				return c
 	return null
+
+func autocompletePoints(line):
+	var openChar = []
+	for c in line:
+		if c in openBracket:
+			openChar.append(c)
+		else:
+			openChar.pop_back()
+	
+	var pointsTotal = 0
+	while !openChar.empty():
+		var c = openChar.pop_back()
+		pointsTotal *= 5
+		pointsTotal += autocompleteCharTable[c]
+	return pointsTotal
